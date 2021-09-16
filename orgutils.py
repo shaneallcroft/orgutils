1493749222b358f5af61dda1325652451361fddc
Shane Allcroft <shaneallcroft@gmail.com>
2021-09-15 21:34:29 -0400

    fixed some behavior regarding trailing whitespace on header lines and the weird requirement of having the org file start with a newline character

--- orgutils.py
+++ orgutils.py
@@ -1,10 +1,14 @@
-# org file has to start with single/or multiple newline
-def orgToDict(filename, string='!!orgutilsdefault!!', level=1):
-    if not string=='!!orgutilsdefault!!':
+
+# orgToDict:
+# takes in a path to an org file, returns a hierarchical dictionary structured identical to the org file.
+# Org headings are used as string keys in the dictionary
+# -------------------------------------------------------------------------------------------------------
+def orgToDict(filename, string=None, level=1):
+    if not string == None:
         data = string
     else:
         with open(filename) as f:
-            data = f.read()        
+            data = '\n' + f.read()
     full_dict = {}
     first_layer = data.split('\n'+('*' * level)+' ')[1:]
     for item in first_layer:
@@ -15,10 +19,12 @@ def orgToDict(filename, string='!!orgutilsdefault!!', level=1):
         base_value_index = 1
         if not split_item[1].startswith('*'):
             # base case reached
-            full_dict[split_item[0]] = ''
+            full_dict[split_item[0].strip()] = ''
             while base_value_index < len(split_item) and not split_item[base_value_index].startswith('*'):
-                full_dict[split_item[0]] += split_item[base_value_index].strip()
+                full_dict[split_item[0].strip()] += split_item[base_value_index].strip()
                 base_value_index += 1
-            continue                
-        full_dict[split_item[0]] = orgToDict(string='\n'+'\n'.join(split_item[base_value_index:]), level=level+1)
+            continue
+        full_dict[split_item[0].strip()] = orgToDict(filename=filename,
+                                             string='\n'+'\n'.join(split_item[base_value_index:]),
+                                             level=level+1)
     return full_dict
