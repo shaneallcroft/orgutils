b41d7780df3048f04a8e55de273ec98a569acae0
Shane Allcroft <shaneallcroft@gmail.com>
2021-11-05 10:09:43 -0400

    phased out dictToHtml

--- src/orgutils/orgutils.py
+++ src/orgutils/orgutils.py
@@ -79,87 +79,6 @@ def dictToOrg(org_data, output_filename, level=1):
         f.close()
 
 
-
-
-def dictToHtml(dict_data, title='', level=1, full_document=False):
-    html_string = ''
-    if level == 1 and full_document:
-        html_string = '<!DOCTYPE html>\n<html>\n<head>\n<script>'
-        # credit to https://github.com/maqboolkhan/Collapsible-list/blob/master/ul.js for the js embedded in this script
-        # vvvvvvvvvvvvvvvvvv
-        html_string += 'window.onload = function  () {\n'
-        html_string += '	var li_ul = document.querySelectorAll(".col_ul li  ul");\n'
-        html_string += '    for (var i = 0; i < li_ul.length; i++) {\n'
-        html_string += '        li_ul[i].style.display = "none"\n'
-        html_string += '    };\n'
-        html_string += '\n'
-        html_string += '    var exp_li = document.querySelectorAll(".col_ul li > span");\n'
-        html_string += '    for (var i = 0; i < exp_li.length; i++) {\n'
-        html_string += '        exp_li[i].style.cursor = "pointer";\n'
-        html_string += '        exp_li[i].onclick = showul;\n'
-        html_string += '    };\n'
-        html_string += '    function showul () {\n'
-        html_string += '        nextul = this.nextElementSibling;\n'
-        html_string += '        if(nextul.style.display == "block")\n'
-        html_string += '            nextul.style.display = "none";\n'
-        html_string += '        else\n'
-        html_string += '            nextul.style.display = "block";\n'
-        html_string += '    }\n'
-        html_string += '}\n'
-        html_string += '\n</script>'
-        
-        html_string += '<meta name="orghtml" content="width=device-width, initial-scale=1">\n'
-        #html_string += '.collapsible {\n'
-        #html_string += 'background-color: #777;color: white; cursor: pointer;'
-        #html_string += 'padding: 18px; width: 100%; width: 100%; border: none;'
-        #html_string += 'text-align: left; outline: none; font-size: 15px;}\n'
-        #html_string += '.active, .collapsible:hover{ background-color: #555;}\n'
-        #html_string += '.content {padding: 0 18px; max-height:0; overflow:hidden; transition: max-height 0.2s ease-out;'
-        html_string += '</head>\n<body>\n<style>\n'
-        html_string += '        body {\n'
-        html_string += '            font-family: "Roboto", sans-serif;\n'
-        html_string += '            font-size: 17px;\n'
-        html_string += '            background-color: #fdfdfd;\n'
-        html_string += '        }\n'
-        html_string += '        .shadow {\n'
-        html_string += '            box-shadow: 0 4px 2px -2px rgba(0, 0, 0, 0.1);\n'
-        html_string += '        }\n'
-        html_string += '        .btn-danger {\n'
-        html_string += '            color: #fff;\n'
-        html_string += '            background-color: #f00000;\n'
-        html_string += '            border-color: #dc281e;\n'
-        html_string += '        }\n'
-        html_string += '        .masthead {\n'
-        html_string += '            background: #3398E1;\n'
-        html_string += '            height: auto;\n'
-        html_string += '            padding-bottom: 15px;\n'
-        html_string += '            box-shadow: 0 16px 48px #E3E7EB;\n'
-        html_string += '            padding-top: 10px;\n'
-        html_string += '        }\n'
-        html_string += '    </style>\n'
-        html_string += '<ul class="col_ul">\n'
-
-        
-    #if not title == '':
-    #    html_string += '<h2>' + title + '</h2>\n'
-    
-    # Check for base case
-    if isinstance(dict_data, dict):
-        for key in dict_data.keys():
-            html_string += '<li> <span>' + str(key) + '</span>\n<ul>'
-            html_string += dictToHtml(dict_data[key],
-                               title,
-                               level+1,
-                               full_document)
-            html_string += '</ul>\n </li>\n' 
-    else:
-        # Base case reached
-        html_string += '<li>' + str(dict_data) + '</li>\n'
-
-    
-    html_string += '</body>'
-    #u
-    return html_string
     
     
 
@@ -234,7 +153,6 @@ class OrgNode:
         self.tab = '    '
         self.separator = '\n'
         self.translationCode = ''
-        self.kickOffNumber = -1
         lines = content.split('\n')
         self.rawContent = content
         line_count = 0
@@ -354,6 +272,7 @@ class OrgNode:
             else:
                 current_function_string += self.getValue().replace('---R->', 'func' + str(OrgNode.counter))
             number_before = OrgNode.counter
+            difference_count = 0
             self.translationCode  = ''
             
             match_added = False
@@ -363,7 +282,8 @@ class OrgNode:
                 isUniversal = '---u->' in org_node.key
                 if isUniversal or not org_node.isTranslator:
                     OrgNode.counter += 1
-                    current_function_string += (self.tab) + 'ret_str += func'+ str(OrgNode.counter) + '(node_to_translate)\n'
+                    current_function_string += (self.tab) + 'ret_str += func'+ str(OrgNode.counter)
+                    current_function_string += '(node_to_translate)\n'
                 self.translationCode += org_node.generateTranslationCode()
                 # TODO add code for other cases like match or simple key matching
 
